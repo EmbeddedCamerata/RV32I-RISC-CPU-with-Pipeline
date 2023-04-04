70a64fc538a7a63a20f0fa30846210738200652e
KafCoppelia <k740677208@gmail.com>
2023-04-04 16:33:12 +0800

    ✅ chore: update SIM_VALUE25 test check

--- test/top_tb.sv
+++ test/top_tb.sv
@@ -8,6 +8,9 @@ module top_tb;
 
 	top u_top(.*);
 
+	typedef enum {TEST_OK, TEST_FAILED, TEST_UNKNOWN} test_status;
+
+	test_status test_s = TEST_UNKNOWN;
 	string test_base_dir = "./test";
 	string fst_name;
 	string mem_base_dir = {test_base_dir, "/mem"};
@@ -48,6 +51,8 @@ module top_tb;
 
 `ifdef SIM_VALUE25
 		#5000;
+		if (test_s == TEST_UNKNOWN)
+			$display("Simlulation failed!");
 `else
 		#100000;
 `endif /* SIM_VALUE25 */
@@ -83,14 +88,32 @@ module top_tb;
 `ifdef SIM_VALUE25
 	always @(posedge clk) begin
 `ifndef USE_PIPELINE
-		if (u_top.PC == 'h4c) begin
+		if (u_top.PC == 32'h4c && u_top.dmem.we == 1'b1) begin
 			// When PC == 76, the next data & address to write should be 25 & 100
-			if (u_top.DataAdr == 100 && u_top.WriteData == 25)
+			if (u_top.DataAdr == 32'h64 && u_top.WriteData == 32'h19) begin
+				test_s = TEST_OK;
 				$display("Simulation OK: DataAdr=0x%02x, WriteData=0x%02x",
 					u_top.DataAdr, u_top.WriteData);
-			else
+			end
+			else begin
+				test_s = TEST_FAILED;
 				$display("Simlulation failed: DataAdr=0x%02x, WriteData=0x%02x",
 					u_top.DataAdr, u_top.WriteData);
+			end
+		end
+`else
+		if (u_top.PC == 32'h5 && u_top.dmem.we == 1'b1) begin
+			// When PC == 84, the next data & address to write should be 25 & 100
+			if (u_top.DataAdr == 32'h64 && u_top.WriteData == 32'h19) begin
+				test_s = TEST_OK;
+				$display("Simulation OK: DataAdr=0x%02x, WriteData=0x%02x",
+					u_top.DataAdr, u_top.WriteData);
+			end
+			else begin
+				test_s = TEST_FAILED;
+				$display("Simlulation failed: DataAdr=0x%02x, WriteData=0x%02x",
+					u_top.DataAdr, u_top.WriteData);
+			end
 		end
 `endif /* USE_PIPELINE */
 	end
